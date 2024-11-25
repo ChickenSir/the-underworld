0a957f03f5bee1e86d908f0ece2e3bc862313997
ChickenSir <shyguyman11@gmail.com>
2024-11-25 21:04:49 +1100

    Initial mod directory setup

---
+++ src/client/java/com/theunderworld/TheUnderworldClient.java
@@ -0,0 +1,10 @@
+package com.theunderworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheUnderworldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
