c5373b21a03a54227919628d6e89c1800572133f
ChickenSir <shyguyman11@gmail.com>
2024-11-26 17:56:45 +1100

    Add dreadwood door and trapdoor to datagen

--- src/main/java/com/theunderworld/datagen/TUModelProvider.java
+++ src/main/java/com/theunderworld/datagen/TUModelProvider.java
@@ -47,6 +47,8 @@ public class TUModelProvider extends FabricModelProvider {
         dreadwood_pool.slab(BlocksCollection.DREADWOOD_SLAB);
         dreadwood_pool.button(BlocksCollection.DREADWOOD_BUTTON);
         dreadwood_pool.pressurePlate(BlocksCollection.DREADWOOD_PRESSURE_PLATE);
+        blockStateModelGenerator.registerDoor(BlocksCollection.DREADWOOD_DOOR);
+        blockStateModelGenerator.registerTrapdoor(BlocksCollection.DREADWOOD_TRAPDOOR);
     }
 
     @Override
