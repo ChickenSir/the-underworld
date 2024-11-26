34647351ca2c05aec81573c23ca7204ba6b8a373
ChickenSir <shyguyman11@gmail.com>
2024-11-26 18:40:19 +1100

    Add render layers for dreadwood door and trapdoor

--- src/client/java/com/theunderworld/TheUnderworldClient.java
+++ src/client/java/com/theunderworld/TheUnderworldClient.java
@@ -1,10 +1,16 @@
 package com.theunderworld;
 
+import com.theunderworld.Collections.BlocksCollection;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class TheUnderworldClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(BlocksCollection.DREADWOOD_DOOR, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(BlocksCollection.DREADWOOD_TRAPDOOR, RenderLayer.getCutout());
 	}
 }
