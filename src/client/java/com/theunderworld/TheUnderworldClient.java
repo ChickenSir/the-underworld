8b8c24dba8448bbce309e08edb50646ff912434c
ChickenSir <shyguyman11@gmail.com>
2024-11-29 17:56:05 +1100

    Add underworld grass colour provider

--- src/client/java/com/theunderworld/TheUnderworldClient.java
+++ src/client/java/com/theunderworld/TheUnderworldClient.java
@@ -4,6 +4,7 @@ import com.theunderworld.Collections.BlocksCollection;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
 
 public class TheUnderworldClient implements ClientModInitializer {
@@ -12,5 +13,7 @@ public class TheUnderworldClient implements ClientModInitializer {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		BlockRenderLayerMap.INSTANCE.putBlock(BlocksCollection.DREADWOOD_DOOR, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(BlocksCollection.DREADWOOD_TRAPDOOR, RenderLayer.getCutout());
+
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintindex) -> 0x3495eb, BlocksCollection.UNDERWORLD_GRASS);
 	}
 }
