2e47fa404171ba90f7bb26914568562c8bd969f9
ChickenSir <shyguyman11@gmail.com>
2024-11-30 15:08:58 +1100

    Add dreadwood tree to registry

--- src/main/java/com/theunderworld/world/TUConfiguredFeatures.java
+++ src/main/java/com/theunderworld/world/TUConfiguredFeatures.java
@@ -3,6 +3,7 @@ package com.theunderworld.world;
 import com.theunderworld.TheUnderworld;
 import com.theunderworld.Collections.BlocksCollection;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -14,10 +15,13 @@ import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.trunk.ForkingTrunkPlacer;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
 public class TUConfiguredFeatures {
+    // Trees
     public static final RegistryKey<ConfiguredFeature<?, ?>> HOLLOW_OAK_TREE = RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(TheUnderworld.modID, "hollow_oak_tree"));
+    public static final RegistryKey<ConfiguredFeature<?, ?>> DREADWOOD_TREE = RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(TheUnderworld.modID, "dreadwood_tree"));
 
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable) {
         featureRegisterable.register(HOLLOW_OAK_TREE, new ConfiguredFeature<>(Feature.TREE, 
@@ -26,6 +30,16 @@ public class TUConfiguredFeatures {
                 new StraightTrunkPlacer(4, 2, 0), 
                 BlockStateProvider.of(BlocksCollection.HOLLOW_OAK_LEAVES), 
                 new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3), 
+                new TwoLayersFeatureSize(1, 0, 1)
+            ).build()
+        ));
+
+        featureRegisterable.register(DREADWOOD_TREE, new ConfiguredFeature<>(Feature.TREE, 
+            new TreeFeatureConfig.Builder(
+                BlockStateProvider.of(BlocksCollection.DREADWOOD_LOG), 
+                new ForkingTrunkPlacer(4, 2, 0), 
+                BlockStateProvider.of(Blocks.AIR), 
+                new BlobFoliagePlacer(ConstantIntProvider.create(0), ConstantIntProvider.create(0), 0), 
                 new TwoLayersFeatureSize(1, 0, 1)
             ).build()
         ));
