7a9ed418d38563b89e5354d7049c277d1f7fbf22
ChickenSir <shyguyman11@gmail.com>
2024-11-30 15:02:35 +1100

    Add hollow oak sapling to lang file and item group

--- src/main/java/com/theunderworld/TheUnderworld.java
+++ src/main/java/com/theunderworld/TheUnderworld.java
@@ -65,6 +65,7 @@ public class TheUnderworld implements ModInitializer {
 			entries.add(BlocksCollection.UNDERWORLD_GRASS);
 			entries.add(BlocksCollection.UNDERWORLD_SOIL);
 			entries.add(BlocksCollection.HOLLOW_OAK_LEAVES);
+			entries.add(BlocksCollection.HOLLOW_OAK_SAPLING);
 		})
 		.build();
 
