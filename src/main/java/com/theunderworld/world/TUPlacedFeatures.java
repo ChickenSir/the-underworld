ae49d1208211557ef3de15d27945d67cc1317004
ChickenSir <shyguyman11@gmail.com>
2024-12-03 20:59:49 +1100

    Add dreadwood forest placed feature

--- src/main/java/com/theunderworld/world/TUPlacedFeatures.java
+++ src/main/java/com/theunderworld/world/TUPlacedFeatures.java
@@ -18,10 +18,12 @@ public class TUPlacedFeatures {
     // Trees
     public static final RegistryKey<PlacedFeature> HOLLOW_OAK_TREE_PLACED = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(TheUnderworld.modID, "hollow_oak_tree_placed"));
     public static final RegistryKey<PlacedFeature> HOLLOW_OAK_FOREST_PLACED = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(TheUnderworld.modID, "hollow_oak_forest_placed"));
+    public static final RegistryKey<PlacedFeature> DREADWOOD_FOREST_PLACED = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(TheUnderworld.modID, "dreadwood_forest_placed"));
     
     public static void bootstrap(Registerable<PlacedFeature> featureRegisterable) {
         RegistryEntryLookup<ConfiguredFeature<?, ?>> registryEntryLookup = featureRegisterable.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
         RegistryEntry<ConfiguredFeature<?, ?>> hollowOakTree = registryEntryLookup.getOrThrow(TUConfiguredFeatures.HOLLOW_OAK_TREE);
+        RegistryEntry<ConfiguredFeature<?, ?>> dreadwoodTree = registryEntryLookup.getOrThrow(TUConfiguredFeatures.DREADWOOD_TREE);
 
         featureRegisterable.register(HOLLOW_OAK_TREE_PLACED, new PlacedFeature(hollowOakTree, 
             VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
@@ -33,6 +35,12 @@ public class TUPlacedFeatures {
             VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
                 PlacedFeatures.createCountExtraModifier(10, 0.1f, 1), 
                 BlocksCollection.HOLLOW_OAK_SAPLING)
+        ));
+
+        featureRegisterable.register(DREADWOOD_FOREST_PLACED, new PlacedFeature(dreadwoodTree, 
+            VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                PlacedFeatures.createCountExtraModifier(10, 0.1f, 1), 
+                BlocksCollection.DREADWOOD_SHRUB)
         ));
     }
 }
